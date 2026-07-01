142e446baa1a015dc622265436e47db3d5663ad5
Developer <dev@example.com>
2026-07-01 18:51:30 +0600

    Revert "Fix overlap on Segment 08 by increasing node separation"
    This reverts commit f727ac15d362ce49427e23c54aa85ca8d975eb32.

--- frontend/src/pages/CompilerSegment08.jsx
+++ frontend/src/pages/CompilerSegment08.jsx
@@ -25,7 +25,7 @@ const CompilerSegment08 = () => {
   }, []);
 
   const { layoutedNodes, layoutedEdges } = useMemo(() => {
-    const { nodes, edges } = getLayoutedElements(initialNodes, initialEdges, direction, 300);
+    const { nodes, edges } = getLayoutedElements(initialNodes, initialEdges, direction);
     return { layoutedNodes: nodes, layoutedEdges: edges };
   }, [direction]);
 
