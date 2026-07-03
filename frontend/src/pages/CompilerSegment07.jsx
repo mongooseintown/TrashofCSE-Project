ee44d14dfc537f8c064c27421e3bd8de764ffe85
Developer <dev@example.com>
2026-07-03 21:04:31 +0600

    Insert solution/answer image directly below Q10 in Segment 07 previous questions

--- frontend/src/pages/CompilerSegment07.jsx
+++ frontend/src/pages/CompilerSegment07.jsx
@@ -163,6 +163,16 @@ const CompilerSegment07 = () => {
               className="note-handnote-img" 
             />
           </div>
+          <div style={{ marginTop: '1.5rem', borderTop: '2px dashed rgba(255, 91, 34, 0.3)', paddingTop: '1.5rem' }}>
+            <span className="note-image-caption" style={{ color: '#ff5b22', fontWeight: 'bold' }}>Answer / Solution:</span>
+            <div className="note-img-wrapper">
+              <img 
+                src="/cg-seg7-expr-ast-ans.jpg" 
+                alt="Expression translation answer" 
+                className="note-handnote-img" 
+              />
+            </div>
+          </div>
         </div>
 
         {/* SET C - Questions 11 to 14 */}
