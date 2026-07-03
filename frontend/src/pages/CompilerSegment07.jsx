ac9bcd32a3d6c5bf9f34ea4ee9be5a9bb6873f33
Developer <dev@example.com>
2026-07-03 20:57:50 +0600

    Insert solution/answer image directly below Q12 in Segment 07 previous questions

--- frontend/src/pages/CompilerSegment07.jsx
+++ frontend/src/pages/CompilerSegment07.jsx
@@ -196,6 +196,16 @@ const CompilerSegment07 = () => {
               className="note-handnote-img" 
             />
           </div>
+          <div style={{ marginTop: '1.5rem', borderTop: '2px dashed rgba(255, 91, 34, 0.3)', paddingTop: '1.5rem' }}>
+            <span className="note-image-caption" style={{ color: '#ff5b22', fontWeight: 'bold' }}>Answer / Solution:</span>
+            <div className="note-img-wrapper">
+              <img 
+                src="/cg-seg7-expr-dag-ans.jpg" 
+                alt="Expression syntax tree and DAG answer" 
+                className="note-handnote-img" 
+              />
+            </div>
+          </div>
         </div>
 
         <div className="note-image-card">
